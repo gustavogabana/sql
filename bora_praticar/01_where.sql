acec6e70be7d886bce209a92e1f5e4d4829b2f4e
gustavogabana <gustavo_97@protonmail.com>
2025-09-04 15:48:02 -0300

    exercicios

--- bora_praticar/01_where.sql
+++ bora_praticar/01_where.sql
@@ -18,3 +18,54 @@ SELECT *
 FROM produtos
 WHERE DescProduto LIKE '%churn%' -- pior forma
 OR DescCateogriaProduto = 'churn_model'; -- melhor forma
+
+-- selecione todas as transacoes de 1 ponto
+SELECT *
+FROM transacoes
+WHERE QtdePontos = 1;
+
+-- selecione as transacoes realizadas no fim de semana
+SELECT 
+    IdTransacao,
+    DtCriacao,
+    strftime('%w', datetime(substr(DtCriacao, 1, 19))) AS diaSemana
+FROM transacoes
+WHERE diaSemana IN ('0', '6');
+
+-- selecione lista de cliente com zero pontos
+SELECT 
+    IdCliente,
+    QtdePontos
+FROM clientes
+WHERE QtdePontos = 0;
+
+-- lista de clientes com 100 e 200 pontos (inclusive)
+SELECT 
+    IdCliente,
+    QtdePontos
+FROM clientes
+WHERE QtdePontos >= 100 AND QtdePontos <= 200;
+
+-- lista de produtos que o nome começa com "venda de"
+SELECT *
+FROM produtos
+WHERE DescProduto LIKE 'venda de%';
+
+-- lista de produtos que terminam com "lover"
+SELECT *
+FROM produtos
+WHERE DescCateogriaProduto = 'lovers';
+
+-- lista de produtos que são chapéu
+SELECT *
+FROM produtos
+WHERE DescProduto LIKE '%chapéu%';
+
+-- lista de transações com produto "pônei"
+SELECT *
+FROM produtos
+WHERE DescCateogriaProduto = 'ponei';
+
+SELECT *
+FROM transacao_produto
+WHERE IdProduto = 15;
